ec55c4d941c1c55c871a9ce719f0be3e0f99804d
Adam Benda <bendaad1@fit.cvut.cz>
2016-10-02 10:30:03 +0200

    Lot of new data sources, yay!

--- web/public_html/data/src/script.sh
+++ web/public_html/data/src/script.sh
@@ -29,14 +29,52 @@ paste /tmp/"$$"_a /tmp/"$$"_lan /tmp/"$$"_c  /tmp/"$$"_lon  /tmp/"$$"_e |sed -e 
 echo '</gpx>' >> ../$type.gpx
 done
 
+#did not use: swimming_pool
+for type in bbq platform stop_position parking recycling toilets viewpoint firepit picnic_site playground railway_station subway_entrance tram_stop sports_center sport_pitch nature_reserve waste_basket highway_bus_stop #drinking_water was merged with studanky and created manually
 
-for type in drinking_water
 do
 #<node id="25314361" lat="50.1318907" lon="14.2944820">
 echo '<?xml version="1.0" encoding="utf-8"?>' > ../$type.gpx
 echo '<gpx xmlns="http://www.topografix.com/GPX/1/1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd" version="1.1">"' >>  ../$type.gpx
 
- grep "<node" xapi/$type |sed -e 's/<node/<wpt/' | sed -e 's/$/<\/wpt>/' >> ../$type.gpx
+ grep "<node" xapi/$type.xml |sed -e 's/<node/<wpt/' | sed -e 's/$/<\/wpt>/' >> ../$type.gpx
 
 echo '</gpx>' >> ../$type.gpx
 done
+
+#merging:
+#merge platform and stop_position and railway_station subway_entrance tram_stop as mhd_stop
+#todo: add also bus_stop
+
+resultName="mhd_stop";
+echo '<?xml version="1.0" encoding="utf-8"?>' > ../$resultName.gpx
+echo '<gpx xmlns="http://www.topografix.com/GPX/1/1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd" version="1.1">"' >>  ../$resultName.gpx
+for type in platform stop_position railway_station subway_entrance tram_stop highway_bus_stop
+ do
+ grep "<wpt" ../$type.gpx >> ../$resultName.gpx 
+done
+echo '</gpx>' >> ../$resultName.gpx
+
+#merge sports_center sport_pitch into sport
+resultName="sport";
+echo '<?xml version="1.0" encoding="utf-8"?>' > ../$resultName.gpx
+echo '<gpx xmlns="http://www.topografix.com/GPX/1/1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd" version="1.1">"' >>  ../$resultName.gpx
+for type in sports_center sport_pitch
+ do
+ grep "<wpt" ../$type.gpx >> ../$resultName.gpx 
+done
+echo '</gpx>' >> ../$resultName.gpx
+
+
+
+#merge bbq + picnic_site + zidle into sitting
+resultName="sitting";
+echo '<?xml version="1.0" encoding="utf-8"?>' > ../$resultName.gpx
+echo '<gpx xmlns="http://www.topografix.com/GPX/1/1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd" version="1.1">"' >>  ../$resultName.gpx
+for type in bbq picnic_site 
+ do
+ grep "<wpt" ../$type.gpx >> ../$resultName.gpx 
+done
+grep "wpt" ./zidle_wgs.gpx >> ../$resultName.gpx
+echo '</gpx>' >> ../$resultName.gpx
+
