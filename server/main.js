167584b192714d53ab8ce67d95f592f6ff8bf372
Adam Benda <adam@adasek.cz>
2016-10-18 13:19:28 +0200

    Test 1000 random points

--- server/main.js
+++ server/main.js
@@ -12,21 +12,20 @@ var SpatialArray = require('./class/SpatialArray');
 var osluneni = JSON.parse(fs.readFileSync('./data/HM_Ekola_den_p.json'));
 //var osluneni = JSON.parse(fs.readFileSync('./data/OVZ_Klima_Osluneni_p.json'));
 
-var point = {
-    "type": "Feature",
-    "geometry": {
-        "type": "Point",
-        "coordinates": [14.3968919, 50.0815478]
-    }
-};
-
 /*
- for (var f_key in osluneni.features) {
- if (osluneni.features[f_key].geometry.type !== "Polygon") {
- console.log(osluneni.features[f_key]);
+ var point = {
+ "type": "Feature",
+ "geometry": {
+ "type": "Point",
+ "coordinates": [14.3968919, 50.0815478]
  }
- }
+ };
  */
+
+var testPoints = (turf.random('point', 1000, {'bbox': [14.3242211, 50.0507947, 14.5278114, 50.1012511]}));
+
+var testRes1 = [];
+var testRes2 = [];
 
 var polygonCount = 1000000; //todo: count polygons prior 
 
@@ -39,25 +38,33 @@ for (f_key in osluneni.features) {
 }
 osluneniA.insertingFinished();
 
-//Search with O(n)
 var startTime = new Date();
-var nearest = null;
-for (f_key in osluneni.features) {
-    feature = osluneni.features[f_key];
 
-    if (turf.inside(point, feature)) {
-        nearest = feature;
+var point = null;
+var nearest = null;
+for (var i = 0; i < testPoints.features.length; i++) {
+    point = testPoints.features[i];
+    nearest = null;
+    for (f_key in osluneni.features) {
+        feature = osluneni.features[f_key];
+
+        if (turf.inside(point, feature)) {
+            nearest = feature;
+            break;
+        }
     }
+    testRes1.push(nearest);
 }
-//console.log(nearest);
-console.log(JSON.stringify(nearest.properties));
+
 var endTime = new Date();
 
 
 
 var startTime2 = new Date();
-var found = osluneniA.findItem(point.geometry.coordinates).features[0];
-console.log(JSON.stringify(found.properties));
+
+for (var i = 0; i < testPoints.features.length; i++) {
+    testRes2.push(osluneniA.findItem(testPoints.features[i].geometry.coordinates).features[0]);
+}
 var endTime2 = new Date();
 
 console.log("O(n) approach " + (endTime - startTime));
